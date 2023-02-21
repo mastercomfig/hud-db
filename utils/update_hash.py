7a0fe4c4a729d363b8699ae96a78d94afad0fc33
mcoms <mastercoms@tuta.io>
2023-02-21 14:58:54 -0500

    allow specifying HUDs to update

--- utils/update_hash.py
+++ utils/update_hash.py
@@ -1,14 +1,28 @@
 import json
+import sys
 import subprocess
 
 from pathlib import Path
 
 data = Path("hud-data/")
 
+if len(sys.argv) > 1:
+  globs = []
+  for arg in sys.argv[1:]:
+    arg = arg.strip()
+    if arg.endswith(".json") or arg == "*.json":
+      globs.append(arg)
+    else:
+      globs.append(f"{arg}.json")
+else:
+  print("Must specify globs")
+  sys.exit(1)
+
 huds = {}
-for f in data.glob("*.json"):
-  with open(f) as file:
-    huds[f.stem] = json.load(file)
+for glb in globs:
+  for f in data.glob(glb):
+    with open(f) as file:
+      huds[f.stem] = json.load(file)
 
 for hud_id, hud in huds.items():
   hud["hash"] = subprocess.check_output(["git", "ls-remote", hud["repo"], "HEAD"]).decode("utf-8").split()[0]
