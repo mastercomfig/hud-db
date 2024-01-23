4079f5f64351d60c9595a6f1039a423add46c65f
mastercoms <mastercoms@tuta.io>
2024-01-23 14:09:08 -0500

    hud updates

--- utils/update_hash.py
+++ utils/update_hash.py
@@ -7,27 +7,38 @@ from pathlib import Path
 data = Path("hud-data/")
 
 if len(sys.argv) > 1:
-  globs = []
-  args = str(" ".join(sys.argv[1:])).split()
-  for arg in args:
-    if arg.endswith(".json") or arg == "*.json":
-      globs.append(arg)
-    else:
-      globs.append(f"{arg}.json")
+    globs = []
+    args = str(" ".join(sys.argv[1:])).split()
+    for arg in args:
+        if arg.endswith(".json"):
+            globs.append(arg)
+        else:
+            globs.append(f"{arg}.json")
 else:
-  print("Must specify globs")
-  sys.exit(1)
+    print("Must specify globs")
+    sys.exit(1)
 
 huds = {}
 for glb in globs:
-  for f in data.glob(glb):
-    with open(f) as file:
-      huds[f.stem] = json.load(file)
+    for f in data.glob(glb):
+        with open(f) as file:
+            huds[f.stem] = json.load(file)
+
+if len(huds) == 0:
+    print(
+        f"No HUDs found. Your arguments passed were: {sys.argv[1:]}. Did you mean for that?"
+    )
+    sys.exit(1)
 
 for hud_id, hud in huds.items():
-  hud["hash"] = subprocess.check_output(["git", "ls-remote", hud["repo"], "HEAD"]).decode("utf-8").split()[0]
+    if hud["repo"].startswith("https://github.com/"):
+        hud["hash"] = (
+            subprocess.check_output(["git", "ls-remote", hud["repo"], "HEAD"])
+            .decode("utf-8")
+            .split()[0]
+        )
 
 for hud_id, hud in huds.items():
-  with open(data / f"{hud_id}.json", "w") as file:
-    json.dump(hud, file, indent=2)
-    file.write('\n')
+    with open(data / f"{hud_id}.json", "w") as file:
+        json.dump(hud, file, indent=2)
+        file.write("\n")
